bb3719369e20285f859600d2023b4fca1a2823ff
johannbm <johannes.bredholt.moskvil@nav.no>
2023-09-18 13:00:56 +0200

    add missing .js suffix

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { addProxyHandler } from "./apiProxy.js";
 export { requireEnvironment } from "./config.js";
-export { addLocalViteServerHandler, addServeSpaHandler } from "./singlePageAppUtils";
+export { addLocalViteServerHandler, addServeSpaHandler } from "./singlePageAppUtils.js";

6397f306624102942b8cc31912076900cacc775c
johannbm <johannes.moskvil@gmail.com>
2024-08-30 14:42:24 +0200

    lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,33 +5,33 @@ import eslintPluginUnicorn from "eslint-plugin-unicorn";
 import tseslint from "typescript-eslint";
 
 const IGNORED_UNICORN_RULES = {
-    "unicorn/filename-case": "off",
-    "unicorn/no-null": "off",
-    "unicorn/prevent-abbreviations": "off",
-    "unicorn/no-nested-ternary": "off",
+  "unicorn/filename-case": "off",
+  "unicorn/no-null": "off",
+  "unicorn/prevent-abbreviations": "off",
+  "unicorn/no-nested-ternary": "off",
 };
 
 export default tseslint.config(
-    {
-        ignores: ["dist/*"],
+  {
+    ignores: ["dist/*"],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
     },
-    eslint.configs.recommended,
-    ...tseslint.configs.recommended,
-    {
-        plugins: {
-            "simple-import-sort": simpleImportSort,
-        },
-        rules: {
-            "simple-import-sort/imports": "error",
-            "simple-import-sort/exports": "error",
-        },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
     },
-    eslintPluginUnicorn.configs["flat/recommended"],
-    {
-        rules: {
-            eqeqeq: ["error", "always"],
-            ...IGNORED_UNICORN_RULES,
-        },
+  },
+  eslintPluginUnicorn.configs["flat/recommended"],
+  {
+    rules: {
+      eqeqeq: ["error", "always"],
+      ...IGNORED_UNICORN_RULES,
     },
-    eslintPluginPrettierRecommended,
+  },
+  eslintPluginPrettierRecommended,
 );
